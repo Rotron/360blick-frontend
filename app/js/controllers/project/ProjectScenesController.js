d8b6b16facb8d9e4f41c55b5a895079479f1343f
Joscha Probst <jprobst.mmt-b2012@fh-salzburg.ac.at>
2015-03-08 17:53:40 +0100

    fetch all scenes of project

--- app/js/controllers/project/ProjectScenesController.js
+++ app/js/controllers/project/ProjectScenesController.js
@@ -1,5 +1,17 @@
 'use strict';
 
-app.controller('ProjectScenesController', ['$scope', '$stateParams', function ($scope, $stateParams) {
+app.controller('ProjectScenesController', ['$scope', '$stateParams', 'RequestService', function ($scope, $stateParams, RequestService) {
     $scope.username = $stateParams.username;
+
+    function getAllScenes(){
+        RequestService.post('scenes/get_scenes', {project: {id: $stateParams['projectId']}}, function(res) {
+                $scope.scenes = res.data;
+            }, function(error) {
+                console.log(error);
+            }
+        );
+    }
+
+    getAllScenes();
+
 }]);
