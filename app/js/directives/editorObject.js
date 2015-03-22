f2d595d92546bbda140f6f37fc127b821abac70e
Joscha Probst <jprobst.mmt-b2012@fh-salzburg.ac.at>
2015-03-22 20:30:20 +0100

    extracted editor object to directive

---
+++ app/js/directives/editorObject.js
@@ -0,0 +1,11 @@
+app.directive('editorObject', [function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'partials/editorObject.html',
+        replace: true,
+        link: function(scope, elem, attrs) {
+
+
+        }
+    };
+}]);
