6064bc4da412198b030c50830251fd078ed72892
David Neubauer <davidneub@gmail.com>
2015-04-11 16:13:45 +0200

    added basic projects/assets support

--- app/js/controllers/project/ProjectAssetsController.js
+++ app/js/controllers/project/ProjectAssetsController.js
@@ -1,5 +1,18 @@
 'use strict';
 
-app.controller('ProjectAssetsController', ['$scope', '$stateParams', function ($scope, $stateParams) {
+app.controller('ProjectAssetsController', ['$scope', '$stateParams', 'RequestService', function ($scope, $stateParams, RequestService) {
     $scope.username = $stateParams.username;
+
+    function getAllProjectAssets(){
+        RequestService.post('projects/assets/get_from_project', {project: {id: $stateParams['projectId']}}, function(res) {
+                console.log(res.data);
+                $scope.assets = res.data;
+            }, function(error) {
+                console.log(error);
+            }
+        );
+    }
+
+    getAllProjectAssets();
+
 }]);
