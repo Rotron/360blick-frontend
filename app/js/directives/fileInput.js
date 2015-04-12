b61a8823ecc617743957aae247f5f7a2960fd269
David Neubauer <davidneub@gmail.com>
2015-04-12 21:11:51 +0200

    added file upload

---
+++ app/js/directives/fileInput.js
@@ -0,0 +1,51 @@
+app.directive('fileInput', ['$timeout', 'RequestService', function ($timeout, RequestService) {
+    return {
+        restrict: 'E',
+        templateUrl: 'partials/fileInput.html',
+        replace: true,
+        scope: {
+            value: '=',
+            label: '@',
+            classSuffix: '@',
+            uploadData: '@'
+        },
+        link: function($scope, elem, attrs) {
+            $scope.isFocused = false;
+
+/*            $scope.onChange = function() {
+            };
+
+            $scope.onBlur = function() {
+                $scope.isFocused = false;
+            };
+
+            $scope.setFocus = function() {
+                elem[0].querySelector('input').focus();
+                $scope.isFocused = true;
+            };*/
+
+            $scope.upload = function(files, event) {
+
+                angular.forEach(files, function(file) {
+                    // FIXME: remove hardcoded
+                    // var data = $scope.uploadData;
+                    var data = {project: {id: 1}};
+                    var fileReader = new FileReader();
+
+                    fileReader.readAsBinaryString(file);
+                    fileReader.onload = function(event) {
+                        RequestService.upload('projects/assets/create', data, file, function(res) {
+                                console.log(res);
+                            }, function(error) {
+                                console.log(error);
+                            }, function(progressPercentage, event) {
+                                console.log('Progress: ' + progressPercentage, event.config.file.name);
+                            }
+                        );
+                    };
+                });
+
+            };
+        }
+    };
+}]);
