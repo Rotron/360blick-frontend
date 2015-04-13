efe05042e733252f3ed077f1d75f01e0cdf5deb9
David Neubauer <davidneub@gmail.com>
2015-04-13 13:07:13 +0200

    added support for karma test, added basic LoginController tests

--- karma.conf.js
+++ karma.conf.js
@@ -1,26 +1,27 @@
 // Karma configuration
 // http://karma-runner.github.io/0.10/config/configuration-file.html
 
+'use strict';
+
 module.exports = function(config) {
   config.set({
     // base path, that will be used to resolve files and exclude
     basePath: '',
+
+    plugins: [
+        'karma-phantomjs-launcher',
+        'karma-jasmine'
+    ],
 
     // testing framework to use (jasmine/mocha/qunit/...)
     frameworks: ['jasmine'],
 
     // list of files / patterns to load in the browser
     files: [
-      'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/bower_components/angular-resource/angular-resource.js',
-      'app/bower_components/angular-cookies/angular-cookies.js',
-      'app/bower_components/angular-sanitize/angular-sanitize.js',
-      'app/bower_components/angular-route/angular-route.js',
-      'app/scripts/*.js',
-      'app/scripts/**/*.js',
-      'test/mock/**/*.js',
-      'test/spec/**/*.js'
+        'build/scripts/vendor.js',
+        'build/scripts/main.min.js',
+        'app/bower_components/angular-mocks/angular-mocks.js',
+        'test/**/*.js'
     ],
 
     // list of files / patterns to exclude
@@ -46,7 +47,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
