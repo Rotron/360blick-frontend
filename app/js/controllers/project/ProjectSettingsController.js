bc30afb65703f3361e31b69ae2f5cae2e98432cc
David Neubauer <davidneub@gmail.com>
2015-05-27 02:06:50 +0200

    added support for export project

--- app/js/controllers/project/ProjectSettingsController.js
+++ app/js/controllers/project/ProjectSettingsController.js
@@ -1,6 +1,20 @@
 'use strict';
 
-app.controller('ProjectSettingsController', ['$scope', '$stateParams', function ($scope, $stateParams) {
+app.controller('ProjectSettingsController', ['$scope', '$stateParams', 'RequestService', function ($scope, $stateParams, RequestService) {
     $scope.username = $stateParams.username;
     $scope.projectId = $stateParams.projectId;
+
+    $scope.generateExport = function($event) {
+        $event.stopPropagation();
+
+        $scope.exportUrl = null;
+
+        RequestService.post('projects/export/zip', {project_id: $scope.projectId}, function(res) {
+                $scope.exportUrl = res.data;
+            }, function(error) {
+                console.log(error);
+            }
+        );
+    };
+
 }]);
