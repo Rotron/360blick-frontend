961f295be32502eca777dad8d4be35fc07f7d714
David Neubauer <davidneub@gmail.com>
2015-05-28 01:12:00 +0200

    added export-list with download support

--- app/js/controllers/project/ProjectSettingsController.js
+++ app/js/controllers/project/ProjectSettingsController.js
@@ -1,8 +1,13 @@
 'use strict';
 
-app.controller('ProjectSettingsController', ['$scope', '$stateParams', 'RequestService', function ($scope, $stateParams, RequestService) {
+app.controller('ProjectSettingsController', ['$scope', '$stateParams', 'RequestService', '$rootScope', 'ENV_CONFIG', function ($scope, $stateParams, RequestService, $rootScope, ENV_CONFIG) {
     $scope.username = $stateParams.username;
     $scope.projectId = $stateParams.projectId;
+    $scope.exports = [];
+
+    $scope.startDownload = function(item) {
+        window.open(ENV_CONFIG.assets + item.path.slice(6, item.path.length), 'Download');
+    };
 
     $scope.generateExport = function($event) {
         $event.stopPropagation();
@@ -11,10 +16,18 @@ app.controller('ProjectSettingsController', ['$scope', '$stateParams', 'RequestS
 
         RequestService.post('projects/export/zip', {project: {id: $scope.projectId}}, function(res) {
                 $scope.exportUrl = res.data;
+                $rootScope.$broadcast('newExport', res.data);
             }, function(error) {
                 console.log(error);
             }
         );
     };
+
+    RequestService.post('projects/export/get_zip_files', {project: {id: $scope.projectId}}, function(res) {
+            $scope.exports = res.data.userZipFiles;
+        }, function(error) {
+            console.log(error);
+        }
+    );
 
 }]);
