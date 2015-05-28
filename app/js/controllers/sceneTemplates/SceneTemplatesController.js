b278b47519312272a09028f7dcc40fb84135a7e9
David Neubauer <davidneub@gmail.com>
2015-05-29 00:35:23 +0200

    fixed some logic for tempaltes query flow

--- app/js/controllers/sceneTemplates/SceneTemplatesController.js
+++ app/js/controllers/sceneTemplates/SceneTemplatesController.js
@@ -16,8 +16,19 @@ app.controller('SceneTemplatesController', ['$scope', '$rootScope', '$stateParam
 
     getAllTemplates();
 
+    $scope.deleteTemplate = function(scene, $event) {
+        $event.stopPropagation();
+
+        RequestService.post('templatescenes/delete', {scene: {id: scene.id}}, function (res) {
+                $rootScope.$broadcast('removeTemplate', res.data);
+            }, function (error) {
+                console.log(error);
+            }
+        );
+    };
+
     $rootScope.$on('removeTemplate', function(event, data) {
-        $scope.projects.splice($scope.sceneTemplates.indexOf(data), 1);
+        $scope.sceneTemplates.splice($scope.sceneTemplates.indexOf(data), 1);
     });
 
     $rootScope.$on('newTemplate', function(event, data) {
