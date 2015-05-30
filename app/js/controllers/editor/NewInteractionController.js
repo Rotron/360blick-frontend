45978ea8ba4e4e57151ff95437f5c2041ca818f3
Joscha Probst <jprobst.mmt-b2012@fh-salzburg.ac.at>
2015-05-30 14:43:32 +0200

    added select objects in interactionController

--- app/js/controllers/editor/NewInteractionController.js
+++ app/js/controllers/editor/NewInteractionController.js
@@ -3,10 +3,30 @@
   app.controller('NewInteractionController', ['$scope', '$stateParams', 'SUPPORTED_INTERACTIONS', 'RequestService', '$filter', function ($scope, $stateParams, SUPPORTED_INTERACTIONS, RequestService, $filter) {
 
       $scope.projectId = $stateParams['projectId'];
-      $scope.username = $stateParams['username']
+      $scope.username = $stateParams['username'];
 
       $scope.interactions = SUPPORTED_INTERACTIONS;
       $scope.currentInteractionId = null;
+
+      function getSceneObjects() {
+          var objects = $scope.data.item.parent.children;
+          var reducedObjects = [];
+          for(var i = 0; i < objects.length; i++) {
+              if(objects[i].type === 'Mesh' && objects[i].id !== $scope.data.item.id) {
+                  reducedObjects.push({
+                      id: objects[i].id,
+                      title: 'Mesh ' + objects[i].id
+                  })
+              }
+          }
+          return reducedObjects;
+      }
+
+      $scope.sceneObjects = getSceneObjects();
+      $scope.currentObjectId = $scope.data.item.id;
+
+      $scope.scenes = [];
+      $scope.currentSceneId = $stateParams['sceneId'];
 
       $scope.onInteractionSelect = function(id) {
           $scope.currentInteractionId = id;
