aa1ca71a3d93d18094bbb1525055734ab1b35de7
Julian Schwarz <jschwarz.mmt-b2012@fh-salzburg.ac.at>
2015-05-31 10:54:19 +0200

    fixed user settings

--- app/js/controllers/user/UserSettingsController.js
+++ app/js/controllers/user/UserSettingsController.js
@@ -1,7 +1,20 @@
 'use strict';
 
-app.controller('UserSettingsController', ['$scope', '$stateParams', 'RequestService', function ($scope, $stateParams, RequestService) {
+app.controller('UserSettingsController', ['$scope', '$stateParams', 'RequestService', '$rootScope', function ($scope, $stateParams, RequestService, $rootScope) {
     $scope.username = $stateParams.username;
+
+    $scope.uploadOptions = {
+        broadcastDomain: 'updatedUserImage',
+        apiEndPoint: 'users/update',
+        paramName: 'data[asset][file]',
+        uploadData: {},
+        modalHeader: 'New Preview Image'
+    };
+
+    $rootScope.$on('updatedUserImage', function(event, data) {
+        console.log('new project image:', data);
+        $scope.user.profile_image = data.profile_image;
+    });
 
     // Get user settings
     RequestService.post('users/get_data', {}, function(res) {   
