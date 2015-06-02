cff1d799a18621f9c315283376a7a369af1dd0b6
David Neubauer <davidneub@gmail.com>
2015-06-02 15:07:12 +0200

    session renew on user update, closes #35

--- app/js/controllers/dialog/ErrorController.js
+++ app/js/controllers/dialog/ErrorController.js
@@ -2,7 +2,7 @@
 
 app.controller('ErrorController', ['$scope', function ($scope) {
     $scope.message = $scope.data.error || 'Unexpected Error';
-    $scope.title = $scope.data.title || '';
+    $scope.title = $scope.data.title || 'Error';
 
     $scope.okCallback = function () {
         $scope.data.okCallback && $scope.data.okCallback();
--- app/js/controllers/user/UserSettingsController.js
+++ app/js/controllers/user/UserSettingsController.js
@@ -3,7 +3,6 @@
 app.controller('UserSettingsController', ['$scope', 'SessionService', 'RequestService', '$rootScope', 'ENV_CONFIG', function ($scope, SessionService, RequestService, $rootScope, ENV_CONFIG) {
 
     $scope.user = {
-        nick: SessionService.nick,
         email: SessionService.email,
         profileImage: ENV_CONFIG.assets + SessionService.profileImage
     };
@@ -17,11 +16,28 @@ app.controller('UserSettingsController', ['$scope', 'SessionService', 'RequestSe
         maxFiles: 1
     };
 
+    // TODO: Move to SessionService
+    var updateSession = function(data) {
+        SessionService.destroy();
+        SessionService.create(data.token, data.nick, data.email, data.role, data.profile_image);
+    };
+
     $rootScope.$on('updatedUserImage', function(event, data) {
+        updateSession(res);
         $scope.user.profileImage = ENV_CONFIG.assets + data.profile_image;
     });
 
     $scope.updateUserSettings = function($event) {
+        $event.stopPropagation();
 
+        // FIXME: not able to change email?? Bachend condition?
+/*        RequestService.post('users/update', {email: $scope.email}, function(res) {
+                updateSession(res.data);
+                // TODO: bind data
+                $scope.user.email = res.data.email;
+            }, function(error) {
+                console.log(error);
+            }
+        );*/
     };
 }]);
