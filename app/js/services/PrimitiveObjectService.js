be00f5e8ff736e698b0c1614830a5def1c17637f
Joscha Probst <jprobst.mmt-b2012@fh-salzburg.ac.at>
2015-06-03 01:46:47 +0200

    Add Text support in Editor, closes #67

--- app/js/services/PrimitiveObjectService.js
+++ app/js/services/PrimitiveObjectService.js
@@ -98,8 +98,16 @@ app.service('PrimitiveObjectService',['RequestService', 'ENV_CONFIG', 'CameraSer
         object.custom = {
             id: properties.id
         };
-        if(properties && properties.interaction) {
+        if(properties.interaction) {
             object.custom.interaction = JSON.parse(properties.interaction) || null;
+        }
+        if(object.geometry && object.geometry.type == 'TextGeometry') {
+            if(properties.text) {
+                this.changeText(properties.text, object);
+                object.custom.text = properties.text;
+            } else {
+                object.custom.text = 'Text';
+            }
         }
         return object;
     };
@@ -125,6 +133,17 @@ app.service('PrimitiveObjectService',['RequestService', 'ENV_CONFIG', 'CameraSer
         if ($rootScope && $rootScope.$$phase != '$apply' && $rootScope.$$phase != '$digest') {
             $rootScope.$apply();
         }
+    };
+
+    this.changeText = function(text, object) {
+        object.geometry =  new THREE.TextGeometry( text, {
+            font: 'helvetiker',
+            weight: 'normal',
+            height: 1,
+            style : 'normal',
+            size: 1,
+            divisions: 1
+        });
     };
     /************************************ /copy to player (objectLoader) ************************************************/
 
