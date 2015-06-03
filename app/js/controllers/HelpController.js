e4f3a0cf58be48d054de50c2bd0f2fb716476e71
David Neubauer <davidneub@gmail.com>
2015-06-03 03:22:17 +0200

    optimized help modal

--- app/js/controllers/HelpController.js
+++ app/js/controllers/HelpController.js
@@ -1,7 +1,12 @@
 'use strict';
 
-app.controller('HelpController', ['$scope', 'RequestService', 'ModalService', function ($scope, RequestService, ModalService) {
+app.controller('HelpController', ['$scope', 'RequestService', 'ModalService', 'SessionService', function ($scope, RequestService, ModalService, SessionService) {
     $scope.credentials = {};
+
+    if(SessionService.email) {
+        $scope.credentials.sender = SessionService.email;
+    }
+
     $scope.sendHelp = function(credentials) {
           RequestService.post('messages/feedback', {sender: credentials.sender, header: $scope.credentials.subject, body: credentials.message}, function(res) {
                   ModalService.openModal('info', {title: 'Success', message: 'Message sent. We will answer you as soon as possible!'});
