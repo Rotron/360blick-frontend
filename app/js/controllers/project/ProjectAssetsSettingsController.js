1af89fec9e3e6ae94b2bd3c4d45c4c2813a936dc
Julian Schwarz <jschwarz.mmt-b2012@fh-salzburg.ac.at>
2015-06-03 03:08:02 +0200

    added projects asets settings

--- app/js/controllers/project/ProjectAssetsSettingsController.js
+++ app/js/controllers/project/ProjectAssetsSettingsController.js
@@ -1,6 +1,24 @@
 'use strict';
 
-app.controller('ProjectAssetsSettingsController', ['$scope', '$stateParams', 'RequestService', '$rootScope', 'ENV_CONFIG', function ($scope, $stateParams, RequestService, $rootScope, ENV_CONFIG) {
+app.controller('ProjectAssetsSettingsController', ['$scope', '$stateParams', 'RequestService', '$rootScope', 'ENV_CONFIG', 'ModalService', function ($scope, $stateParams, RequestService, $rootScope, ENV_CONFIG, ModalService) {
 
+    RequestService.post('projects/assets/specific', {asset_id: $stateParams.assetId}, function(res) {
+          $scope.assetTitle = res.data.title;
+        }, function(error) {
+            console.log(error);
+        }
+    );
+
+    $scope.updateAsset = function($event) {
+        $event.stopPropagation();
+
+        RequestService.post('projects/assets/update', {id: $stateParams.assetId, title: $scope.assetTitle}, function(res) {
+                $scope.assetTitle = res.data.title;
+                ModalService.openModal('info', {title: 'Success', message: 'Successfully updated.'});
+            }, function(error) {
+                console.log(error);
+            }
+        );
+    };
 
 }]);
